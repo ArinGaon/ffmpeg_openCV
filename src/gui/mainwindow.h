e24367742c4b0b4d09edd954703102273328c5af
unknown <binnara1000@naver.com>
2024-11-22 07:31:56 +0900

    Qt gui file directory 생성

---
+++ src/gui/mainwindow.h
@@ -0,0 +1,32 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+
+QT_BEGIN_NAMESPACE
+namespace Ui {
+    class MainWindow;
+}
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+// 버튼 클릭시 호출된 함수 (private)
+private slots:
+    void on_startRecordButton_clicked();
+    void on_stopRecordButton_clicked();
+    void on_startStreamButton_clicked();
+    void on_stopStreambutton_clicked();
+
+    // 클릭 함수기에 clicked로 선언하고 Qt Creater에 만들어놓은 블럭명과 일치시킴
+
+
+private:
+    Ui::MainWindow *ui;
+};
+#endif // MAINWINDOW_H
